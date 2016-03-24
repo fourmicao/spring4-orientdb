83b251f29d8d6da51353a3ab8609daa9a01549c2
Das <X148128@hrblock.com>
2016-03-24 13:47:51 +0000

    Adding Annotation view resolver config

--- src/main/java/com/hrb/config/SpringConfig.java
+++ src/main/java/com/hrb/config/SpringConfig.java
@@ -1,14 +1,12 @@
 package com.hrb.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @PropertySources({
         @PropertySource("classpath:application.properties")
 })
+@Import(ViewResolverConfiguration.class)
 @Configuration
 public class SpringConfig {
 
---
+++ src/main/java/com/hrb/config/ViewResolverConfiguration.java
@@ -0,0 +1,43 @@
+package com.hrb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * Created by x148128 on 24/03/2016.
+ */
+@Configuration
+public class ViewResolverConfiguration {
+    /*
+    <bean id="viewResolver"
+          class="org.springframework.web.servlet.view.UrlBasedViewResolver">
+        <property name="viewClass"
+                  value="org.springframework.web.servlet.view.JstlView"/>
+        <property name="prefix" value="/WEB-INF/jsp/"/>
+        <property name="suffix" value=".jsp"/>
+    </bean>
+     */
+
+    @Bean
+    public UrlBasedViewResolver urlBasedViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setCache(false);
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    //make it a bean to use InternalResourceViewResolver.
+    //@Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
